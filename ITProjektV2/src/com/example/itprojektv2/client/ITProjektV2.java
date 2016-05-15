c66847aaac77d5e95854fc9e80523a4f75e5d0c3
Tilmann <TilmannVogel>
2016-05-15 13:22:42 +0200

    HelloWorld Test hinzugefügt, SuperDevMode funktioniert jetzt.

--- ITProjektV2/src/com/example/itprojektv2/client/ITProjektV2.java
+++ ITProjektV2/src/com/example/itprojektv2/client/ITProjektV2.java
@@ -1,6 +1,5 @@
 package com.example.itprojektv2.client;
 
-import com.example.itprojektv2.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -8,6 +7,7 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
@@ -28,14 +28,19 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  * 
  */
 
-
 public class ITProjektV2 implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		
-		Button 
+		    Button b = new Button("Click me", new ClickHandler() {
+		      public void onClick(ClickEvent event) {
+		        Window.alert("Hello, AJAX");
+		      }
+		    });
+
+		    RootPanel.get().add(b);
+		  }
 		
 	}
 	
-}
+
